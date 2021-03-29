b9d00f45d5b4a2e0c86f30a0454ec9b3da859c45
martinarmstrong <martin.armstrong@digital.hmrc.gov.uk>
2021-03-29 16:40:32 +0100

    GG-5525 library updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "simple-reactivemongo"           % "7.30.0-play-27",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.58.0-play-27",
-    "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-27",
-    "uk.gov.hmrc"       %% "play-ui"                        % "8.13.0-play-27",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"      % "3.0.0"
+    "uk.gov.hmrc"       %% "simple-reactivemongo"           % "8.0.0-play-27",
+    "uk.gov.hmrc"       %% "logback-json-logger"            % "5.1.0",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.65.0-play-27",
+    "uk.gov.hmrc"       %% "play-health"                    % "3.16.0-play-27",
+    "uk.gov.hmrc"       %% "play-ui"                        % "9.1.0-play-27",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"      % "3.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -20,7 +20,7 @@ object AppDependencies {
     "org.pegdown"                 %  "pegdown"                  % "1.6.0"                   % "test, it",
     "org.jsoup"                   %  "jsoup"                    % "1.13.1"                  % "test, it",
     "org.mockito"                 %  "mockito-core"             % "3.2.4"                   % "test, it",
-    "uk.gov.hmrc"                 %% "service-integration-test" % "0.10.0-play-27"          % "test, it"
+    "uk.gov.hmrc"                 %% "service-integration-test" % "1.1.0-play-27"          % "test, it"
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
