d41d5e0977ff271c9e0c03982e82406dbfb006cc
szabolcs-szilagyi-hmrc <131175010+szabolcs-szilagyi-hmrc@users.noreply.github.com>
2023-05-10 16:22:59 +0100

    restrict the scope of the test lib of flexmark

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
     "com.typesafe.play"           %% "play-test"                % PlayVersion.current       % "test, it",
     "org.scalatest"               %% "scalatest"                % "3.1.4"                   % "test, it",
     "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0"                   % "test, it",
-    "com.vladsch.flexmark"        % "flexmark-all"              % "0.36.8",
+    "com.vladsch.flexmark"        % "flexmark-all"              % "0.36.8"                  % "test, it",
     "org.jsoup"                   %  "jsoup"                    % "1.15.3"                  % "test, it",
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.12"                % "test, it",
     "uk.gov.hmrc"                 %% "service-integration-test" % "1.3.0-play-28"          % "test, it"
