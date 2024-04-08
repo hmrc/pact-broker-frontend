fc1439005a82ac56efcfa67eb43865cf8428edfc
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2024-04-08 13:45:03 +0100

    GG-7601 migrated to play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,20 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVer = "8.4.0"
+  private val bootstrapVer = "8.5.0"
   private val hmrcMongoVer = "1.7.0"
 
   private val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVer,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVer
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVer,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVer
   )
 
   private val test = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28"  % bootstrapVer,
+    "uk.gov.hmrc"         %% "bootstrap-test-play-30"  % bootstrapVer,
     "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.17.0",
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8",
     "org.mockito"         %% "mockito-scala-scalatest" % "1.17.30"
-  ).map(_ % "test, it")
+  ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
 }
